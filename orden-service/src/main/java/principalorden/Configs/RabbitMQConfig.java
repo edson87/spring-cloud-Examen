23c76c235810bf8b7b1795cfc533470a56266324
Rubén Soria <rusok.software@gmail.com>
2025-08-29 20:37:21 -0400

    Event Driven Architecture 3 - Terminando RabbitMQ

--- orden-service/src/main/java/principalorden/Configs/RabbitMQConfig.java
+++ orden-service/src/main/java/principalorden/Configs/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,4 +28,17 @@ public class RabbitMQConfig {
     public Binding binding(Queue queue, TopicExchange exchange){
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
+    // Este bean define el convertidor JSON para mensajes
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    // Este bean segura que el RabbitTemplate use el convertidor JSON
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+       RabbitTemplate template = new RabbitTemplate(connectionFactory);
+       template.setMessageConverter(messageConverter);
+       return template;
+    }
+
 }
--- producto-service/src/main/java/principalproducto/Configs/RabbitMQConfig.java
+++ producto-service/src/main/java/principalproducto/Configs/RabbitMQConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +25,10 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange){
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+    // Este bean acá es para recibir los mensajes JSON
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
