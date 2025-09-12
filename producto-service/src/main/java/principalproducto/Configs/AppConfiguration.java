54a6da5b1e19d97c209a9f2943d5cfb0b98419a5
Rubén Soria <rusok.software@gmail.com>
2025-09-11 22:02:09 -0400

    JWT: Dependencia agregada.

--- producto-service/src/main/java/principalproducto/Configs/AppConfiguration.java
+++ producto-service/src/main/java/principalproducto/Configs/AppConfiguration.java
@@ -2,6 +2,9 @@ package principalproducto.Configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import principalproducto.Filters.JwtRequestFilter;
 
 @Configuration
